d9f493beb3c85032365ee0f841ff80e02715a117
KeyMtz <martinezkey0306@gmail.com>
2022-02-04 16:21:49 -0600

    Laboratorio 1

--- Laboratorios/Laboratorios.R
+++ Laboratorios/Laboratorios.R
@@ -1,3 +1,71 @@
 # KeyMtz
-# 02/02/2022
-# Laboratorio 1 
+# 04/02/2022
+# Laboratorio 1
+
+# Gastos totales
+
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+Celular <- 300
+Transporte <- 240 
+Comestibles <- 1527
+Gimansio <- 400
+Alquiler <- 1500
+Otros <- 1833 
+
+gastos <- c(Celular, Transporte, Comestibles, Gimansio, Alquiler, Otros)
+
+# Base de datos 
+
+"gastos al mes" <- 5800
+
+Celular <- 300
+Transporte <- 240 
+Comestibles <- 1527
+Gimansio <- 400
+Alquiler <- 1500 
+Otros <- 1833
+
+"Gastos totales" 
+ 5800 * 5
+ 5800 * 10
+ 
+ 
+# Variables cualitativas y cuantitativas 
+#Problema 1 
+
+ #Nombre del estudiante -cualitativa
+ #Fecha de nacimineto -cuantitativa
+ #Edad -cuantitativa
+ #Direccion de casa -cualitativa
+ #Numero de telefono -cuantitativa
+ #Area principal de estudio -cualitativa
+ #Grado de año universitario -cuantitativa
+ #Puntaje en la prueba de mitad de periodo -cuantitativa
+ #Calificacion general -cualitativa
+ #Tiempo para completar la prueba final de MCF 202 -cuantitativa
+ #Numero de hermanos -cuantitativa
+ 
+#Problema 2 
+#Animales
+
+"Nombre de especie - cualitativa"
+"Color de pelaje - cualitativa"
+"Edad - cuantitativa"
+"Peso - cuantitativa"
+
+#Problema 3 
+
+Opiniones <-c(1, 2, 3, 4, 5)
+"Twitter, correo electronico, mensaje de texto, facebook, blog"
+"¿Esta es una varieble cualitativa o cuantitativa?"
+#Seria una variable cuantitativa  ya que nos proporciona el numero de opiniones dadas en los diferentes medios de comunicacion
+ 
+#Problema 4 
+
+"¿Cuál es la cantidad promedio de horas que los estudiantes de universidades públicas trabajan cada semana? <-cuantitativa"
+"¿Qué proporción de todos los estudiantes universitarios de México están inscritos en una universidad pública? <- cuantitativa"
+"En los universidades públicas, ¿las estudiantes femeninas tienen un promedio de CENEVAL más alto que los estudiantes varones? <- cuantitativa"
+"¿Es más probable que los atletas universitarios reciban asesoramiento académico que los atletas no universitarios? <- cualitativos"
+"Si reuniéramos datos para responder a las preguntas de la investigación anterior, ¿qué datos podrían analizarse mediante un histograma? ¿Cómo lo sabes? <-cuantitativa"
+  
