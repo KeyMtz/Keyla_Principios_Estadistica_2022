1968ae687460a02311b0da780e1b9b1dd3e51bb4
Keyla <martinezkey0306@gmail.com>
2022-02-10 09:04:35 -0600

    Semana 4 dia 2

---
+++ Clases/ClaseS4D2.R
@@ -0,0 +1,32 @@
+# KeyMtz
+# 10/02/2022
+# Semana 4 dia 2 
+# Descargar datos de internet
+
+url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfo.csv"
+
+
+# fileEncoding = "Latini"
+profepa <- read.csv(url)
+
+summary(profepa)
+
+profepa$Inspeccion >= mean(profepa$Inspeccion)
+
+# subset
+
+ins.alta <- subset(profepa, profepa$Inspeccion >= mean(profepa$Inspeccion))
+ins.baja <- subset(profepa, profepa$Inspeccion <= mean(profepa$Inspeccion))
+
+Est.C <- subset(profepa, profepa$Entidad == "Chihuahua")
+
+Est.C <- profepa[4:8,]
+Est.N <- profepa[17:18, ]
+
+# Obtener los estados con inspecciones a > a 15 pero > a 35
+
+ins.media <- subset(profepa, profepa$Inspeccion >= 15 & profepa$Operativo >= 10)
+rec.media <- subset(profepa, profepa$Recorrido <= mean(profepa$Recorrido) & profepa$Operativo <= mean(profepa$Operativo))
+
+mean(profepa$Recorrido)
+mean(profepa$Operativo)
