4d8b4dd959a4d99976aab2969826d07d4ef6e30b
Keyla <martinezkey0306@gmail.com>
2022-04-28 09:04:37 -0500

    Clase semana 13 dia 1

--- Clases/ClaseS13D1.R
+++ Clases/ClaseS13D1.R
@@ -10,6 +10,7 @@ plot(Semillas$Diametro_mm, Semillas$Peso_gr)
 cor.test(Semillas$Diametro_mm, Semillas$Peso_gr)
 cor.test(Semillas$Diametro_mm, Semillas$Longitud_mm)
 
+
 x <- c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5)
 y <- c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68)
 
